cf9ba0d2ff6e9deaa78ab19937f4eed4a52c6baa
caleb-freitas <calebfreitas@tutanota.com>
2022-08-08 17:10:40 -0300

    fix: added vercel env variable

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,10 +8,22 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
+function getBaseUrl() {
+  if (typeof window !== "undefined") {
+    return "";
+  }
+
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  }
+
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
 export default withTRPC<AppRouter>({
   config({ ctx }) {
     return {
-      url: "/api/trpc",
+      url: `${getBaseUrl()}/api/trpc`,
       transformer: superjson
     };
   },
